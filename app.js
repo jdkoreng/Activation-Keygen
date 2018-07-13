5a2338e846a830349732c0ce3869c07958f71c13
unknown <jeffreykorengold@gmail.com>
2018-07-12 21:11:41 -0600

    dsfdsfds

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
+var activationRouter = require('./routes/activation');
 var app = express();
 
 // view engine setup
@@ -17,14 +16,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', activationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
 });
 
 // error handler
---
+++ teststuff/entercodetest.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+var db = require("../models");
+
+
+
+//post your code and info
+//checks DB to see if exists, and if it does then do the post route
+// and execute send email function
+router.post('/activate', (req, res) => {
+
+    //if req.params.code ===
+    // db.records.find( { req.body.key: { $exists: true } } )
+    //method="POST" action="/api/students/">
+
+    db.keydb.update({
+            alreadyUsed: true,
+            email: req.params.email,
+            firstName: req.params.firstName,
+            lastName: req.params.lastName
+
+        }, {
+            where: {
+                key: req.params.key
+            }
+        })
+        .then(() => {
+            console.log("processed");
+            res.json({message: 'Your account has been activated. Please check your email.'})
+        },
+        function () {
+            res.send("Your activation code is either incorrect or has been used already. Please try again or contact jkorengold@sunsetlearning.com to troubleshoot.");
+        });
+})
+
+module.exports = router;
+
