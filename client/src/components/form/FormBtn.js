3015f951e2f41eaebc6c9640e6c8b90d14b0f5fb
unknown <jeffreykorengold@gmail.com>
2018-07-14 11:37:55 -0600

    dsfdsfdfs

---
+++ client/src/components/form/FormBtn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FormBtn.css";
+
+
+export const FormBtn = props => (
+ 
+<input for="location-search" id="search" type="submit"></input>
+
+
+  );
+
+  export default FormBtn;
---
+++ client/src/components/form/input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./input.css";
+
+const input = props => (
+
+<input type="text" id="bike-search" placeholder="Activation Code"></input>
+<input type="text" id="bike-search" placeholder="Email Address"></input>
+<input type="text" id="bike-search" placeholder="Name"></input>
+
+      );
+
+
+      export default input;
