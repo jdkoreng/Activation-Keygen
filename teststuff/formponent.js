791845baabf839bd90a4602db98dd741459d2e76
unknown <jeffreykorengold@gmail.com>
2018-07-14 10:58:39 -0600

    created components folder and files

---
+++ teststuff/formponent.js
@@ -0,0 +1,7 @@
+<form>
+<input type="text" id="bike-search" placeholder="Activation Code"></input>
+<input type="text" id="bike-search" placeholder="Email Address"></input>
+<input type="text" id="bike-search" placeholder="Name"></input>
+<input for="location-search" id="search" type="submit"></input>
+</form>  
+
